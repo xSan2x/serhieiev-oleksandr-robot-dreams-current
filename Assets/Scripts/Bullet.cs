15abf2d3f1433e76313d651f4709099b0e333d6d
Alexander Sergeyev <73773265+xSan2x@users.noreply.github.com>
2025-03-10 14:40:35 +0200

    [MOD] L12 improved ricochet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,11 +7,15 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float _speed = 10f;
     [SerializeField] float _destructionDelay = 0.5f;
+
+    Rigidbody _rigidbody;
     // Start is called before the first frame update
     void Start()
     {
         //Start coroutine to destroy the bullet after 5 seconds
         StartCoroutine(DestroyBullet(_destructionDelay));
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(transform.forward * _speed, ForceMode.Force);
     }
 
     public void SetDirectionPoint(Vector3 directionPoint)
@@ -20,18 +24,22 @@ public class Bullet : MonoBehaviour
         transform.LookAt(directionPoint);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         //Move the bullet forward
-        transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         //Ricochet the bullet
+        Debug.Log("Bullet velocity before = "+_rigidbody.velocity);
+        Vector3 copyVelocity = _rigidbody.velocity;
+        _rigidbody.velocity = Vector3.zero;
         transform.forward = Vector3.Reflect(transform.forward, collision.contacts[0].normal);
-        _speed /= 2;
-        Debug.Log("Bullet collided with " + collision.gameObject.name);
+        
+        _rigidbody.velocity = copyVelocity / 2;
+        Debug.Log("Bullet velocity after = " + _rigidbody.velocity);
     }
 
     private IEnumerator DestroyBullet(float delay)
